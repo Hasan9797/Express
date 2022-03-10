730430544edb32966aa6bb1f7889717733c774bc
Hasanjon <hasanjonsadullayev97@gmail.com>
2022-03-10 17:13:39 +0500

    Express dvijok

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const Express = require("express");
 const app = Express();
 
-const path = require("path");
+app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-    // res.send('Hello World');
-    // res.status(200).send("Hello chuvak :)");
-    // res.download("index.js");
-    res.sendFile(path.join(__dirname, "views", "index.html"));
+    res.render("index", {title: 'Chuvaaak :)'});
 });
 
 app.listen(5000, () => console.log("Server is running..."))
